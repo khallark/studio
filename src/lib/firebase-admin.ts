be24c231d6b1b668695318cad10d909bec2b1bfc
khallark <khallark295@gmail.com>
2025-09-06 11:57:19 +0530

    modified:   src/lib/firebase-admin.ts

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -2,10 +2,22 @@ import admin from 'firebase-admin';
 // import serviceAccount from '../../service-account.json';
 
 if (!admin.apps.length) {
-  const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY!);
+  const serviceAccount = {
+    type: process.env.FIREBASE_SERVICE_ACC_TYPE,
+    project_id: process.env.FIREBASE_SERVICE_ACC_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_SERVICE_ACC_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_SERVICE_ACC_PRIVATE_KEY,
+    client_email: process.env.FIREBASE_SERVICE_ACC_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_SERVICE_ACC_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_SERVICE_ACC_AUTH_URI,
+    token_uri: process.env.FIREBASE_SERVICE_ACC_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FIREBASE_SERVICE_ACC_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.FIREBASE_SERVICE_ACC_CLIENT_X509_CERT_UR,
+    universe_domain: process.env.FIREBASE_SERVICE_ACC_UNIVERSE_DOMAIN,
+  }
 
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount!),
+    credential: admin.credential.cert(serviceAccount),
     projectId: serviceAccount.project_id,
   });
 }

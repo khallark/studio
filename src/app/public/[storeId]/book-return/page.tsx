0ea4e3208e0b9ca3122bd501a08356dd7e4d9599
khallark <khallark295@gmail.com>
2025-09-28 12:08:26 +0000

    modified:   src/app/public/[storeId]/book-return/page.tsx

--- src/app/public/[storeId]/book-return/page.tsx
+++ src/app/public/[storeId]/book-return/page.tsx
@@ -235,7 +235,7 @@ export default function BookReturnPage() {
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 10V3L4 14h7v7l9-11h-7z" />
             </svg>
             <span className="border-b border-transparent group-hover:border-current">
-              Visit {storeId} Store
+              Visit the Store
             </span>
             <svg className="w-3 h-3 transition-transform group-hover:translate-x-0.5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />

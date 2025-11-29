404d0d9fc2a36bb04294f1385d8a47657d04105b
Kunal Khallar <khallark295@gmail.com>
2025-11-29 20:54:54 +0530

    modified:   src/app/api/shopify/locations/update/route.ts

--- src/app/api/shopify/locations/update/route.ts
+++ src/app/api/shopify/locations/update/route.ts
@@ -2,24 +2,24 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { db, auth as adminAuth } from '@/lib/firebase-admin';
 import { FieldValue } from 'firebase-admin/firestore';
-import { authUserForBusinessAndStore } from '@/lib/authoriseUser';
+import { authUserForBusiness } from '@/lib/authoriseUser';
 
 export async function POST(req: NextRequest) {
   try {
-    const { businessId, shop, locationId, location } = await req.json();
+    const { businessId, locationId, location } = await req.json();
 
     if (!businessId) {
       console.error('Not business id provided');
       return NextResponse.json({ error: 'No business id provided.' }, { status: 400 });
     }
 
-    if (!shop || !locationId || !location) {
-      console.error('Shop, locationId, and location data are required', shop, locationId, location);
-      return NextResponse.json({ error: 'Shop, locationId, and location data are required' }, { status: 400 });
+    if (!locationId || !location) {
+      console.error('locationId, and location data are required', locationId, location);
+      return NextResponse.json({ error: 'locationId, and location data are required' }, { status: 400 });
     }
 
     // ----- Auth -----
-    const result = await authUserForBusinessAndStore({ businessId, shop, req });
+    const result = await authUserForBusiness({ businessId, req });
 
     if (!result.authorised) {
       const { error, status } = result;
@@ -33,21 +33,22 @@ export async function POST(req: NextRequest) {
     }
 
     const { memberDoc } = result
-    const memberData = memberDoc?.data();
-    const memberRole = memberData?.role;
-    if (!memberRole) {
-      return NextResponse.json({ error: 'No member role assigned, assign the member a role.' }, { status: 403 });
-    }
+    const locationRef = memberDoc?.ref.collection('pickupLocations').doc(locationId);
+    // const memberData = memberDoc?.data();
+    // const memberRole = memberData?.role;
+    // if (!memberRole) {
+    //   return NextResponse.json({ error: 'No member role assigned, assign the member a role.' }, { status: 403 });
+    // }
 
-    let locationRef;
-    if (memberRole === 'Vendor') {
-      locationRef = memberDoc?.ref.collection('pickupLocations').doc(locationId);
-    } else if (memberRole === 'SuperAdmin' || memberRole === 'Admin') {
-      const accountRef = db.collection('accounts').doc(shop);
-      locationRef = accountRef.collection('pickupLocations').doc(locationId);
-    } else {
-      return NextResponse.json({ error: 'Forbidden: You do not have permission to update locations.' }, { status: 403 });
-    }
+    // let locationRef;
+    // if (memberRole === 'Vendor') {
+    //   locationRef = memberDoc?.ref.collection('pickupLocations').doc(locationId);
+    // } else if (memberRole === 'SuperAdmin' || memberRole === 'Admin') {
+    //   const accountRef = db.collection('accounts').doc(shop);
+    //   locationRef = accountRef.collection('pickupLocations').doc(locationId);
+    // } else {
+    //   return NextResponse.json({ error: 'Forbidden: You do not have permission to delete locations.' }, { status: 403 });
+    // }
 
     const docSnap = await locationRef?.get();
     if (!docSnap?.exists) {

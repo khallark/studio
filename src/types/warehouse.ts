0df51e79bd298f25bfc1070607cc5773fabcd417
Kunal Khallar <khallark295@gmail.com>
2026-01-28 18:36:47 +0530

    modified:   src/types/warehouse.ts

--- src/types/warehouse.ts
+++ src/types/warehouse.ts
@@ -181,11 +181,11 @@ export type UPC = {
   putAway: "none" | "outbound" | null;
 
   productId: string;
-  warehouseId: string;
-  zoneId: string;
-  rackId: string;
-  shelfId: string;
-  placementId: string;
+  warehouseId: string | null;
+  zoneId: string | null;
+  rackId: string | null;
+  shelfId: string | null;
+  placementId: string | null;
 } | {
   id: string;
   createdAt: Timestamp;

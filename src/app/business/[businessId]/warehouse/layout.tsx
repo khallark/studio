016c4db2f69f7cdd43bec8a3286a8b935114f90a
Kunal Khallar <khallark295@gmail.com>
2026-02-22 17:43:28 +0530

    modified:   src/app/business/[businessId]/warehouse/layout.tsx

--- src/app/business/[businessId]/warehouse/layout.tsx
+++ src/app/business/[businessId]/warehouse/layout.tsx
@@ -250,7 +250,7 @@ export default function WarehouseLayout({
     };
 
     return (
-        <div className="flex min-h-0">
+        <div className="flex items-start">
             {/* Side Menu */}
             <aside className="hidden lg:flex flex-col w-[280px] border-r bg-gradient-to-b from-background via-background to-muted/20">
                 {/* Header */}

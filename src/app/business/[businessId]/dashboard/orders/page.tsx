0bb47ba4cb896499db471f5539fec1356d2e71ac
Kunal Khallar <khallark295@gmail.com>
2026-05-02 21:09:13 +0530

    modified:   src/app/business/[businessId]/dashboard/orders/page.tsx

--- src/app/business/[businessId]/dashboard/orders/page.tsx
+++ src/app/business/[businessId]/dashboard/orders/page.tsx
@@ -1892,7 +1892,7 @@ export default function BusinessOrdersPage() {
                                     <SelectValue />
                                 </SelectTrigger>
                                 <SelectContent>
-                                    {[10, 20, 50, 100, 200].map((size) => (
+                                    {[10, 20, 50].map((size) => (
                                         <SelectItem key={size} value={`${size}`}>{size}</SelectItem>
                                     ))}
                                 </SelectContent>

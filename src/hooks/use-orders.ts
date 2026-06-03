20a90b3d5c8a0859655feb3922cd3b5d52b12bc2
Kunal Khallar <khallark295@gmail.com>
2026-06-03 13:59:49 +0530

    modified:   src/hooks/use-orders.ts

--- src/hooks/use-orders.ts
+++ src/hooks/use-orders.ts
@@ -220,7 +220,7 @@ export function useOrders(
                     filters.courierFilter &&
                     filters.courierFilter !== 'all'
                 ) {
-                    constraints.push(where('courier', '==', filters.courierFilter));
+                    constraints.push(where('courierProvider', '==', filters.courierFilter));
                 }
 
                 if (filters.vendorName) {

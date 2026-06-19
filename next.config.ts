c3c56dc188ad23cc532b3a26bdf74b1d37732521
Kunal Khallar <khallark295@gmail.com>
2026-06-19 13:29:43 +0530

    modified:   next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
   assetPrefix: process.env.NEXT_PUBLIC_ASSET_PREFIX || undefined,
   typescript: {
     ignoreBuildErrors: true,
